a458b6872d4ab3be14ce30a177766357be6419c7
Cecaro <rbcerovic@gmail.com>
2017-02-20 15:33:09 +0100

    Added link to title, comments not working properly The comments do not display for the correct blog post.

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <button id="menu-toggle" class="menu-toggle"><span>Menu</span></button>
 <div id="theSidebar" class="sidebar">
 	<button class="close-button fa fa-fw fa-close"></button>
-	<h1><span>Kids<span> In the Stars</h1>
+	<h1><a href="http://wwww.kidsinthestars.com"><span>Kids<span> In the Stars</a></h1>
 	<div class="description">
 		<h5 class="fr">Maman survoltée d’une tribu de trois princesses parfois démoniaques, d’un bébé modèle Golgoth 12 et d’un chien qui pète : vraiment pas le temps de s’ennuyer !!! </h5>
 		<h5 class="en">Hyperactive mom of a tribe made up of three sometimes diabolical princesses, a humoungus baby and a farting dog : no time to be bored!</h5>
